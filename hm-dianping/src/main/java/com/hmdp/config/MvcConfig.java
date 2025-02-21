5253d2c73f3b17b06c2e2b3759db423507b947e4
whale-coding <njq3699@gmail.com>
2025-02-21 18:56:06 +0800

    使用双拦截器对登录进行优化

--- hm-dianping/src/main/java/com/hmdp/config/MvcConfig.java
+++ hm-dianping/src/main/java/com/hmdp/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.hmdp.config;
 
 import com.hmdp.interceptor.LoginInterceptor;
+import com.hmdp.interceptor.RefreshTokenInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -19,6 +20,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
     // 添加拦截器
     /*
+    // 基于session登录，添加登录拦截器
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())  // 注册自定义的登录拦截器
@@ -37,6 +39,8 @@ public class MvcConfig implements WebMvcConfigurer {
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
+    /*
+    // 基于redis登录，添加登录拦截器
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor(stringRedisTemplate))  // 注册自定义的登录拦截器
@@ -50,4 +54,26 @@ public class MvcConfig implements WebMvcConfigurer {
                         "/user/login"
                 );
     }
+    */
+
+
+    // 基于双拦截器优化登录功能
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // token刷新的拦截器
+        // 拦截所有请求，且通过order()设置其执行在登录拦截器之前
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);
+        // 登录拦截器
+        registry.addInterceptor(new LoginInterceptor())  // 注册自定义的登录拦截器
+                .excludePathPatterns(  // 配置拦截规则。excludePathPatterns是配置要放行哪些接口
+                        "/shop/**",
+                        "/voucher/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/blog/hot",
+                        "/user/code",
+                        "/user/login"
+                ).order(1);
+    }
+
 }
--- hm-dianping/src/main/java/com/hmdp/interceptor/RefreshTokenInterceptor.java
+++ hm-dianping/src/main/java/com/hmdp/interceptor/RefreshTokenInterceptor.java
@@ -2,19 +2,13 @@ package com.hmdp.interceptor;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.StrUtil;
-import cn.hutool.crypto.digest.otp.TOTP;
-import com.hmdp.constants.Constants;
 import com.hmdp.dto.UserDTO;
-import com.hmdp.entity.User;
 import com.hmdp.utils.UserHolder;
-
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -28,64 +22,32 @@ import static com.hmdp.utils.RedisConstants.LOGIN_USER_TTL;
  * @Author: 聂建强
  * @Description: 登录拦截器
  */
-public class LoginInterceptor implements HandlerInterceptor {
+public class RefreshTokenInterceptor implements HandlerInterceptor {
 
     // 前置拦截方法：进入Controller之前，进行登录校验
-
-    /*
-    // 基于session登录
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 1、获取session
-        HttpSession session = request.getSession();
-        // 2、获取session中的用户
-        UserDTO userDTO = (UserDTO) session.getAttribute(Constants.SESSION_USER_KEY);
-        // 3、判断用户是否存在
-        if (userDTO == null){
-            // 4、用户不存在，进行拦截并返回401未授权状态码
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return false;  // 拦截
-        }
-
-        // 5、用户存在，保存用户信息到ThreadLocal中
-        UserHolder.saveUser(userDTO);
-        // 6、放行
-        return true;
-    }
-    */
-
-    /*
 
     // 注意这里不能通过注解注入！！！
     private final StringRedisTemplate stringRedisTemplate;
     // 通过构造器去注入
-    public LoginInterceptor(StringRedisTemplate stringRedisTemplate){
+    public RefreshTokenInterceptor(StringRedisTemplate stringRedisTemplate){
         this.stringRedisTemplate = stringRedisTemplate;
     }
 
-     */
 
-
-    /*
-    // 基于redis登录
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // TODO: 1、获取请求头中的token
         String token = request.getHeader(REQUEST_HEADER_TOKEN_KEY);
         // TODO: 2、判断token是否存在
         if (StrUtil.isBlank(token)){
-            // token不存在，进行拦截并返回401未授权状态码
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return false;  // 拦截
+            return true;  // 放行
         }
         // TODO: 3、基于token获取redis中的用户信息
         String tokenKey = LOGIN_USER_KEY + token;
         Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(tokenKey);
         // TODO: 4、判断用户是否存在
         if (userMap.isEmpty()){
-            // 用户不存在，进行拦截并返回401未授权状态码
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return false;  // 拦截
+            return true;  // 放行
         }
         // TODO: 5、用户存在，将查询到的Hash结构的用户数据转为UserDTO对象
         UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
@@ -98,30 +60,11 @@ public class LoginInterceptor implements HandlerInterceptor {
         // TODO: 8、放行
         return true;
     }
-     */
 
-    // 基于redis登录并使用双拦截器优化
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // TODO: 1、判断是否需要拦截（ThreadLocal中是否有用户）
-        if (UserHolder.getUser() == null){
-            // 没有，需要拦截并返回401未授权状态码
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return false;  // 拦截
-        }
-
-        // TODO: 2、有用户，放行
-        return true;
-    }
-
-    /*
-    //  在基于双拦截器的优化后，登录拦截器中就需要了，在刷新Token拦截器中处理移除用户，防止内存泄露
     // 后置拦截方法：用户业务执行完毕，销毁对应的用户信息
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         // 移除用户，防止内存泄露
         UserHolder.removeUser();
     }
-
-    */
 }
