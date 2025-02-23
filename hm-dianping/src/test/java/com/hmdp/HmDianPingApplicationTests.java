60a6de8d8db16a0b22c320ae9c52b6e3ab91e8ca
whale-coding <njq3699@gmail.com>
2025-02-23 18:45:23 +0800

    Snowflake Algorithm实现全局唯一id

--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -4,6 +4,7 @@ package com.hmdp;
 import com.hmdp.service.impl.ShopServiceImpl;
 
 import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.SnowflakeIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -20,6 +21,8 @@ class HmDianPingApplicationTests {
 
     @Resource
     private RedisIdWorker redisIdWorker;
+
+    SnowflakeIdWorker snowflakeIdWorker = new SnowflakeIdWorker(0,0);
 
     /**
      * 测试往Redis中存入shop数据以及逻辑过期时间
@@ -41,6 +44,29 @@ class HmDianPingApplicationTests {
         Runnable task = () -> {
             for (int i = 0; i < 100; i++) {
                 long id = redisIdWorker.nextId("order");
+                System.out.println("id ="+id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = "+ (end - begin));
+    }
+
+    /**
+     * 测试雪花算法生成分布式ID
+     */
+    @Test
+    public void testSnowflakeIdWorker()throws InterruptedException{
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = snowflakeIdWorker.nextId();
                 System.out.println("id ="+id);
             }
             latch.countDown();
