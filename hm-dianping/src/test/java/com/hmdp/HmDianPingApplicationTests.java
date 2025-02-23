9c11fa4eef45af9d23ca024c08d7795519af1c81
whale-coding <njq3699@gmail.com>
2025-02-23 17:07:57 +0800

    redis实现全局唯一id

--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -3,16 +3,23 @@ package com.hmdp;
 
 import com.hmdp.service.impl.ShopServiceImpl;
 
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
     @Resource
     private ShopServiceImpl shopService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
 
     /**
      * 测试往Redis中存入shop数据以及逻辑过期时间
@@ -21,6 +28,32 @@ class HmDianPingApplicationTests {
     public void test() throws InterruptedException {
         shopService.saveShopToRedis(1L,10L);
     }
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
+    /**
+     * 测试Redis生成分布式ID
+     */
+    @Test
+    public void testIdWorker()throws InterruptedException{
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id ="+id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = "+ (end - begin));
+    }
+
 
 
 }
