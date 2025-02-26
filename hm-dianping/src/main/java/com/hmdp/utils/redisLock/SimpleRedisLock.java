8fcd577d0246b588739f951bdc20623ed01d8c85
whale-coding <njq3699@gmail.com>
2025-02-26 11:31:26 +0800

    解决Redis分布式锁误删问题

--- hm-dianping/src/main/java/com/hmdp/utils/redisLock/SimpleRedisLock.java
+++ hm-dianping/src/main/java/com/hmdp/utils/redisLock/SimpleRedisLock.java
@@ -1,8 +1,9 @@
 package com.hmdp.utils.redisLock;
 
-import io.netty.util.concurrent.SucceededFuture;
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.util.StringTokenizer;
 import java.util.concurrent.TimeUnit;
 
 
@@ -18,6 +19,7 @@ public class SimpleRedisLock implements ILock{
     private StringRedisTemplate stringRedisTemplate;
 
     private static final String KEY_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate){
         this.name = name;
@@ -27,17 +29,25 @@ public class SimpleRedisLock implements ILock{
     @Override
     public boolean tryLock(long timeoutSec) {
         // 获取线程标识
-        long threadId = Thread.currentThread().getId();  // 获取当前线程的id
+        // long threadId = Thread.currentThread().getId();  // 获取当前线程的id
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁
         Boolean success = stringRedisTemplate.opsForValue()
-                .setIfAbsent(KEY_PREFIX + name, threadId + "", timeoutSec, TimeUnit.SECONDS);
+                .setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
         // 避免拆箱时发生空指针异常
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unlock() {
-        // 释放锁
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        // 获取线程标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 获取锁中的标识
+        String lockId = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        // 判断标识是否一致
+        if (threadId.equals(lockId)){
+            // 释放锁
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
