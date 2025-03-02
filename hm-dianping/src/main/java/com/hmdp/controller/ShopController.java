8a114a63df403a830d1d97997eef2e1ef471aeb8
whale-coding <njq3699@gmail.com>
2025-03-02 17:42:16 +0800

    附近商户搜索

--- hm-dianping/src/main/java/com/hmdp/controller/ShopController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/ShopController.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.service.IShopService;
+import com.hmdp.utils.RedisData;
 import com.hmdp.utils.SystemConstants;
 import org.springframework.web.bind.annotation.*;
 
@@ -60,23 +61,24 @@ public class ShopController {
         return shopService.update(shop);
     }
 
+
     /**
-     * 根据商铺类型分页查询商铺信息
+     * 根据商铺类型分页查询商铺信息(附近商铺)
      * @param typeId 商铺类型
-     * @param current 页码
-     * @return 商铺列表
+     * @param current 页码，滚动查询
+     * @param x 经度，非必传参数
+     * @param y 纬度，非必传参数
+     * @return 符合要求的商铺列表
      */
     @GetMapping("/of/type")
     public Result queryShopByType(
             @RequestParam("typeId") Integer typeId,
-            @RequestParam(value = "current", defaultValue = "1") Integer current
+            @RequestParam(value = "current", defaultValue = "1") Integer current,
+            @RequestParam(value = "x", required = false) Double x,
+            @RequestParam(value = "y", required = false) Double y
     ) {
-        // 根据类型分页查询
-        Page<Shop> page = shopService.query()
-                .eq("type_id", typeId)
-                .page(new Page<>(current, SystemConstants.DEFAULT_PAGE_SIZE));
-        // 返回数据
-        return Result.ok(page.getRecords());
+
+        return shopService.queryShopByType(typeId, current, x, y);
     }
 
     /**
--- hm-dianping/src/main/java/com/hmdp/service/IShopService.java
+++ hm-dianping/src/main/java/com/hmdp/service/IShopService.java
@@ -17,4 +17,14 @@ public interface IShopService extends IService<Shop> {
     Result queryById(Long id);
 
     Result update(Shop shop);
+
+    /**
+     * 根据商铺类型分页查询商铺信息(附近商铺)
+     * @param typeId 商铺类型
+     * @param current 页码，滚动查询
+     * @param x 经度，非必传参数
+     * @param y 纬度，非必传参数
+     * @return 符合要求的商铺列表
+     */
+    Result queryShopByType(Integer typeId, Integer current, Double x, Double y);
 }
