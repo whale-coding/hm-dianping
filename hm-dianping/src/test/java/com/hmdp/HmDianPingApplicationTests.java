d78c80407936f635018611310d85c8d30900df3e
whale-coding <njq3699@gmail.com>
2025-03-02 16:50:09 +0800

    导入店铺数据到GEO

--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,17 +1,25 @@
 package com.hmdp;
 
 
+import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.SnowflakeIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -21,6 +29,9 @@ class HmDianPingApplicationTests {
 
     @Resource
     private RedisIdWorker redisIdWorker;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     SnowflakeIdWorker snowflakeIdWorker = new SnowflakeIdWorker(0,0);
 
@@ -78,6 +89,35 @@ class HmDianPingApplicationTests {
         latch.await();
         long end = System.currentTimeMillis();
         System.out.println("time = "+ (end - begin));
+    }
+
+    /**
+     * 导入店铺数据到GEO
+     */
+    @Test
+    public void loadShopData(){
+        // 1.查询店铺信息
+        List<Shop> list = shopService.list();
+        // 2.把店铺分组，按照typeId分组，typeId一致的放到一个集合
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        // 3.分批完成写入redis
+        for (Map.Entry<Long,List<Shop>> entry : map.entrySet()) {
+            // 3.1 获取类型id
+            Long typeId = entry.getKey();
+            String key = "shop:geo:" + typeId;  // 存入redis时的key
+            // 3.2 获取同类型的店铺的集合
+            List<Shop> value = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(value.size());
+            // 3.3 写入redis GEOADD key 经度 纬度 member
+            for (Shop shop : value){
+                // stringRedisTemplate.opsForGeo().add(key, new Point(shop.getX(), shop.getY()), shop.getId().toString());
+                locations.add(new RedisGeoCommands.GeoLocation<>(
+                        shop.getId().toString(),
+                        new Point(shop.getX(), shop.getY())
+                ));
+            }
+            stringRedisTemplate.opsForGeo().add(key,locations);
+        }
     }
 
 
